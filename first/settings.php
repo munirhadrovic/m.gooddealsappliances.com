2ab53433a8f2c0852c36d094d38f6437347e9e00
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-28 14:58:12 +0200

    First commit

---
+++ first/settings.php
@@ -0,0 +1,24 @@
+<?php
+	$adresa[0]="14680-4 South Tamiami Trail Fort Myers,FL 33912"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Fort Myers";
+	$telephone[0]=2394379134;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="1780 Pine Island Road Cape Coral,FL 33909"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Cape Coral";
+	$telephone[1]=2392429328;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="2604 Tamiami Trail North Naples,FL 34103"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Naples";
+	$telephone[2]=2392547630;// Broj koji se poziva na dugme CALL US
+
+	$centratelephone=3052522000;
+	$companynumber=3;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
